066f59bed3620fa6d898ee024eda43942d0ff45c
SimonePerfetto <simone.perfetto94@gmail.com>
2021-11-29 01:31:37 +0000

    added field roll_stats_window to CointPair class; added method to compute hurst exponent for potential future use; added few get methods in Stock and CointPair classes.

--- src/Cointegrator.py
+++ src/Cointegrator.py
@@ -5,7 +5,8 @@ from statsmodels.tsa.api import adfuller
 from src.DataRepository import DataRepository
 import statsmodels.api as sm
 from src.util.OnlineRollingStats import OnlineRollingStats
-
+from hurst import compute_Hc
+from datetime import date
 
 class Stock:
     def __init__(self, ticker: str, repository: DataRepository):
@@ -15,24 +16,32 @@ class Stock:
     def __repr__(self):
         return f'{self.ticker}'
 
-    def get_window_prices(self, current_price_data) -> pd.Series:
+    def get_ticker(self):
+        return self.ticker
+
+    def get_window_prices(self, current_price_data: pd.DataFrame) -> pd.Series:
         return current_price_data[self.ticker]
+
+    def get_todays_price(self, today) -> float:
+        return self.window_prices.loc[pd.to_datetime(today)]
 
 
 class CointPair:
-    def __init__(self, stock_x: Stock, stock_y: Stock, cointegration_result: List):
+    def __init__(self, stock_x: Stock, stock_y: Stock, cointegration_result: List, roll_stats_window: int,
+                 num_std_away: float):
+        self.roll_stats_window = roll_stats_window
+        self.num_std_away: float = num_std_away
         self.stock_x: Stock = stock_x
         self.stock_y: Stock = stock_y
         self.hedge_ratio, self.residuals = cointegration_result
-        self.residuals_mavg: pd.Series = self.residuals.rolling(20).mean()
-        self.residuals_mstdv: pd.Series = self.residuals.rolling(20).std()
-        self.num_std_away: float = 1.5
+        self.residuals_mavg: pd.Series = self.residuals.rolling(self.roll_stats_window).mean()
+        self.residuals_mstdv: pd.Series = self.residuals.rolling(self.roll_stats_window).std()
         self.upper_band: pd.Series = self.residuals_mavg + self.num_std_away * self.residuals_mstdv
         self.lower_band: pd.Series = self.residuals_mavg - self.num_std_away * self.residuals_mstdv
         self.previous_pair_signal: int = 0
+        self.current_pair_signal: int = 0
         self.last_residual, self.last_roll_mean, self.last_roll_std = None, None, None
         self.last_lower_band, self.last_upper_band = None, None
-        self.current_pair_signal: int = 0
 
     def __repr__(self):
         return f"CointPair({self.stock_x.ticker}, {self.stock_y.ticker})"
@@ -45,12 +54,33 @@ class CointPair:
         self.current_pair_signal = self.__get_current_pair_signal(today, no_new_trades_from_date,
                                                                   trade_window_end_date)
 
-    def plot_residuals_and_bb_bands(self, trade_action):
+    def get_prev_signal(self):
+        return self.previous_pair_signal
+
+    def get_curr_signal(self):
+        return self.current_pair_signal
+
+    def set_curr_signal(self, new_signal):
+        self.current_pair_signal = new_signal
+
+    def get_hedge_ratio(self):
+        return self.hedge_ratio
+
+    def get_todays_price_x_y(self, today: date) -> Tuple:
+        px = self.stock_x.get_todays_price(today)
+        py = self.stock_y.get_todays_price(today)
+        return px, py
+
+    def get_ticker_x_y(self):
+        return self.stock_x.get_ticker(), self.stock_y.get_ticker()
+
+    def plot_residuals_and_bb_bands(self, trade_action) -> None:
         residual_features_df = pd.concat([self.residuals, self.residuals_mavg,
                                           self.upper_band, self.lower_band], axis=1).dropna(axis=0)
         residual_features_df.columns = ["Residuals", "Residuals MA", "Residuals Upper BB", "Residuals Lower BB"]
         figure = residual_features_df.iplot(colorscale="polar", theme="white", asFigure=True,
-                                            title=f"{trade_action} {self} - Residuals, BolBands, Residuals MA", xTitle="Time")
+                                            title=f"{trade_action} {self} - Residuals, BolBands, Residuals MA",
+                                            xTitle="Time")
         figure.update_layout(font=dict(family="Computer Modern"))
         # figure.write_image("images/img.pdf", format="pdf")
         figure.show()
@@ -81,8 +111,8 @@ class CointPair:
     def __compute_last_mean_and_std(self, last_residual) -> Tuple:
         mean_old = self.residuals_mavg.values[-1]
         std_old = self.residuals_mstdv.values[-1]
-        first_residual = self.residuals.values[-20]
-        online_roll = OnlineRollingStats(window_size=20, mean=mean_old, stdv=std_old)
+        first_residual = self.residuals.values[-self.roll_stats_window]
+        online_roll = OnlineRollingStats(window_size=self.roll_stats_window, mean=mean_old, stdv=std_old)
         mean_new, std_new = online_roll.update(new=last_residual, old=first_residual)
         return mean_new, std_new
 
@@ -128,8 +158,10 @@ class CointPair:
 
 class Cointegrator:
 
-    def __init__(self, repository: DataRepository):
+    def __init__(self, repository: DataRepository, roll_stats_window: int, num_std_away: float):
         self.repository: DataRepository = repository
+        self.roll_stats_window: int = roll_stats_window
+        self.num_std_away: float = num_std_away
         self.cointegrated_pairs = self.create_cointegrated_pairs()
 
     @staticmethod
@@ -140,7 +172,11 @@ class Cointegrator:
         return hedge_ratio, residuals
 
     @staticmethod
-    def __coint_check(residuals: pd.Series) -> bool:
+    def __get_hurst(res: pd.Series):
+        h, _, _ = compute_Hc(res)
+        return h
+
+    def __coint_check(self, residuals: pd.Series) -> bool:
         '''
         critical values are in the following dictionary form:
             {'1%': -3.4304385694773387,
@@ -150,7 +186,8 @@ class Cointegrator:
         adf_results = adfuller(residuals)
         adf_test_statistic = adf_results[0]
         adf_critical_value = adf_results[4]['1%']
-        return adf_test_statistic < adf_critical_value
+        # hurst = self.__get_hurst(residuals)
+        return adf_test_statistic < adf_critical_value  # and hurst < 0.35
 
     def cointegrate(self, stock_x: Stock, stock_y: Stock) -> Union[Tuple, None]:
         stock_x_coint_window_prices = stock_x.window_prices[:self.repository.window.coint_window_end_date]
@@ -163,8 +200,9 @@ class Cointegrator:
 
     def create_cointegrated_pairs(self) -> List[CointPair]:
         stock_obj_dict, cointpair_stocks, cointpair_list = {}, [], []
-        random.seed(42)
-        shuffled_allowed_couples_raw = random.sample(self.repository.allowed_couples, k=len(self.repository.allowed_couples))
+        random.seed(5)
+        shuffled_allowed_couples_raw = random.sample(self.repository.allowed_couples,
+                                                     k=len(self.repository.allowed_couples))
         shuffled_allowed_couples = self.__remove__class_A_B_share_tickers(shuffled_allowed_couples_raw)
         for tick_x, tick_y in shuffled_allowed_couples:
             if tick_x in cointpair_stocks or tick_y in cointpair_stocks: continue
@@ -173,7 +211,7 @@ class Cointegrator:
             stock_x, stock_y = stock_obj_dict[tick_x], stock_obj_dict[tick_y]
             cointegration_result = self.cointegrate(stock_x, stock_y)
             if cointegration_result is not None:
-                cointpair = CointPair(stock_x, stock_y, cointegration_result)
+                cointpair = CointPair(stock_x, stock_y, cointegration_result, self.roll_stats_window, self.num_std_away)
                 cointpair_stocks += [tick_x, tick_y]
                 cointpair_list.append(cointpair)
         return cointpair_list
